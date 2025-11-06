59281f81f33d994deefc98fcca0fbd54869c77aa
barisyenigun <baris.yenigun35@gmail.com>
2025-11-06 17:13:19 +0300

    Loggings added for checking MQ connection web-test-agent

--- web-test-agent/src/main/java/com/spectra/agent/web/mq/WebJobListener.java
+++ web-test-agent/src/main/java/com/spectra/agent/web/mq/WebJobListener.java
@@ -8,20 +8,18 @@ import org.openqa.selenium.WebDriver;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.util.Map;
-
 @Component
 public class WebJobListener {
     @RabbitListener(queues = "jobs.web")
     public void onMessage(JobCreatedEvent evt) {
-        Map<String, String> config = evt.config();
-        WebDriver driver = WebDriverFactory.create(config);
+        WebDriver driver = WebDriverFactory.create(evt.config());
+        System.out.println(driver);
         StepExecutor stepExecutor = new StepExecutor(driver);
 
         for (StepDTO s : evt.steps()) {
             stepExecutor.executeStep(s);
         }
+
+        System.out.println("StepExecutor executed");
     }
 }
