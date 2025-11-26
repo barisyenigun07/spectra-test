501830b77e5898c1e446999fd775bc40b8c93be4
barisyenigun07 <baris.yenigun35@gmail.com>
2025-11-26 13:48:15 +0300

    SwipeAction for mobile-test-agent module implemented

--- mobile-test-agent/src/main/java/com/spectra/agent/mobile/engine/actions/SwipeAction.java
+++ mobile-test-agent/src/main/java/com/spectra/agent/mobile/engine/actions/SwipeAction.java
@@ -1,12 +1,16 @@
 package com.spectra.agent.mobile.engine.actions;
 
 import com.spectra.agent.mobile.engine.context.ExecutionContext;
+import org.openqa.selenium.Dimension;
 import org.springframework.stereotype.Component;
 
 @Component("swipe")
 public class SwipeAction implements ActionHandler {
     @Override
     public void handle(ExecutionContext ctx) {
-
+        Dimension size = ctx.driver().manage().window().getSize();
+        int startx = (int) (size.width * 0.8);
+        int endx = (int) (size.width * 0.20);
+        int starty = size.height / 2;
     }
 }
